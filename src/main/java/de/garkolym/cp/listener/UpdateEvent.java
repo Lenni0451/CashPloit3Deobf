f73fb4138282f9ebe0a319a096d8a04946a89a39
Lenni0451 <lenni0451@localhost>
2020-08-01 19:39:55 +0200

    Oops, forgot to clean some classes

--- src/main/java/de/garkolym/cp/listener/UpdateEvent.java
+++ src/main/java/de/garkolym/cp/listener/UpdateEvent.java
@@ -3,16 +3,12 @@ package de.garkolym.cp.listener;
 import de.garkolym.cp.Start;
 import de.garkolym.cp.commands.CommandBase;
 
-import java.util.Iterator;
-
 public class UpdateEvent implements Runnable {
-    public void run() {
-        Iterator var2 = Start.INSTANCE.commandManager.commands.iterator();
 
-        while (var2.hasNext()) {
-            CommandBase m = (CommandBase) var2.next();
+    public void run() {
+        for (CommandBase m : Start.INSTANCE.commandManager.commands) {
             m.onTick();
         }
-
     }
+
 }
